fc29488e6b4df436e2861e1083f000be39eca53f
SIK2401 <ruinyst@nate.com>
2021-12-16 10:56:27 +0900

    .
    .

---
+++ bot.py
@@ -0,0 +1,69 @@
+import asyncio, discord
+from discord.ext import commands
+import os
+sttxt = "%"
+bot = commands.Bot(command_prefix=sttxt)
+
+ertxt = "Error - %[인원] [금액] ex) 8인 2000원 일때 -> %8 2000"
+
+
+@bot.event
+async def on_ready():
+	print("We have loggedd in as {0.user}".format(bot))
+
+
+@bot.event
+
+async def on_message(message):
+    # 받은 메시지의 author 가 bot 인 경우 return
+    if message.author.bot:
+        return None
+
+    # 답장할 채널은 메세지 받은 채널로 설정
+
+    channel = message.channel
+
+    if message.content.startswith(sttxt):   # message 의 contest 가 '#커맨드' 로 시작할때     
+        if(message.content.find("죽이실분")==-1):
+            commandr = message.content.split(sttxt)     
+            CountNum = commandr[1].split()
+       
+            try:
+                Person =  float(CountNum[0])
+                prise = float(CountNum[1])
+       
+                if (Person == 0):
+                    Person =1
+                FCost = prise * 0.95;
+                FinalCost = ((FCost * Person) - FCost) / Person
+                TCost = int(FinalCost)         
+                FinalCost2 = (FCost) / Person
+                ICost = int(FinalCost2)
+                BCost = int(FinalCost * 0.9)
+      
+                embed = discord.Embed(title = "결과", description = "손익분기 : " +  f'{TCost}' + " \n\n" +"분배금 : " +  f'{ICost}' + " \n\n" +"1입찰이득 : " +  f'{BCost}' + " \n\n" , color = 0xffc0cb)
+                await channel.send(embed = embed)
+       
+            except :
+                embed = discord.Embed(title = "Error", description = "%[인원] [금액] \n\n" +"ex) 8인 2000원 일때 \n\n" +"%8 2000 \n\n" , color = 0xffc0cb)
+                await channel.send(embed = embed)
+        else :
+            name = message.content.split(sttxt)  
+            strmsg = "저요"
+            if(name[1].find('다')>-1 and name[1].find('윤')>-1):
+                strmsg = "저요 저요 제발 저요"                
+            else:
+                if(name[1].find('던') >-1 and name[1].find('바')>-1):
+                    strmsg = "이이이이잉"
+                else:
+                    strmsg = "저요"
+           
+            await channel.send(strmsg)
+
+      
+    else:   # line29 가 아날때
+       return None
+
+
+    
+bot.run(os.environ['token'])
